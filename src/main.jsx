9b7651a6e3cb81443173f4d17c4d0dd48504cec1
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-06 22:19:18 -0500

    Remove unnecessary imports in Header and main files
    The imports `React` in the `Header.jsx` file and `DevSupport`, `ComponentPreviews`, `useInitial`, and `Layout` in the `main.jsx` file were removed. These weren't being used in the respective files, leading to unnecessary overhead and potential confusion. This cleanup improves code readability and performance.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "./dev/index.js";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./styles/theme.js";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./components/layout/Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
