e47bb50c5a6ac9f328a961cb992c2c2ecdd8bd5e
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-07 13:30:38 -0500

    Add new components and features to the UI
    This commit adds new elements to the Layout2 component, such as "Acceso" and "Registro" buttons and imports necessary icons. It introduces color changes to the List Item Text and in "Salir", making UI more intuitive for users. In the Home component, numerous category links are added and arranged in a grid layout, enhancing the navigation experience. Each link is represented with descriptive icons and distinct colors for better visual cues.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,172 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import HelpIcon from "@mui/icons-material/Help";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import ShopIcon from "@mui/icons-material/Shop";
 
 export default function Home() {
   return (
     <>
-      <Box border={"solid 5px"} borderColor={"violet"}>
-        Home Page
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Link to="/clientes">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#2cd4d7 ",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <PersonSearchIcon sx={{ marginRight: 2 }} />
+                Clientes
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/caja">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#ed8517",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <PointOfSaleIcon sx={{ marginRight: 2 }} />
+                Caja
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/agenda">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#b34dd3",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <CalendarMonthIcon sx={{ marginRight: 2 }} />
+                Agenda
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/productos">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#1fa3cf",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <ShopIcon sx={{ marginRight: 2 }} />
+                Productos
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/stock">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#cea400",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <InventoryIcon sx={{ marginRight: 2 }} />
+                Stock
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/ventas">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#f33636",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <MonetizationOnIcon sx={{ marginRight: 2 }} />
+                Ventas
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/estadisticas">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#f46db1 ",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <QueryStatsIcon sx={{ marginRight: 2 }} />
+                Estadisticas
+              </Paper>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Link to="/configuracion">
+              <Paper
+                sx={{
+                  height: 100,
+                  width: 300,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: " #6b7078 ",
+                  color: "#fcfcfc",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <HelpIcon sx={{ marginRight: 2 }} />
+                Configuracion
+              </Paper>
+            </Link>
+          </Grid>
+        </Grid>
       </Box>
     </>
   );
