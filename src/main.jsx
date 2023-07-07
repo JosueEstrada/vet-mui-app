0ea4930f1189b416542d489274dce8f51d2f9642
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-07 09:43:53 -0500

    add basename

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename="/vet-mui-app/">
+      <BrowserRouter basename="/vet-mui-app">
         <App />
       </BrowserRouter>
     </ThemeProvider>
